786512f9f3a637fe73e0cf24de792126248c3f03
Grace True <gracetrue@Graces-MacBook-Pro-4.local>
2024-04-09 16:44:15 -0700

    pushing all code (contact, date, person all working)

--- test_person_eq_caro.cpp
+++ test_person_eq_caro.cpp
@@ -4,18 +4,24 @@
 
 int main(){
 
+    cout << "P1 ----------------\n";
     Person c1;
     c1.print_person();
-    cout << "----------------\n";
-	
-	
+
+    cout << "P2 ----------------\n";
     Person c2("person_template.txt");
     c2.print_person();
-    /*
+    
+    cout << "P3 ----------------\n";
+    Person c3("person_template_2.txt");
+    c3.print_person();
+    
     cout << "----------------\n";
 
     cout << "Is c1 equal to c2: " << (c1==c2) << endl;
     cout << "Is c1 not equal to c2: " << (c1!=c2) << endl;
-*/
+    cout << "Is c2 equal to c3: " << (c2==c3) << endl;
+    cout << "Is c2 not equal to c3: " << (c2!=c3) << endl;
+    
     return 0;
 }
