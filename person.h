5066fbc095b31171c1674dcc19fffcba12a093dd
20gtrue <79179208+20gtrue@users.noreply.github.com>
2024-04-09 16:47:08 -0700

    Update person.h
    updating person.h with  code from person_caro.h

--- person.h
+++ person.h
@@ -1,17 +1,17 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-// #include "date.h"
+#include "date.h"
 #include "contact.h"
 #include "fstream"
 
 class Person{
-    friend class Network;
+    //friend class Network;
 
 private:
 	string f_name;
 	string l_name;
-	// Date *birthdate;
+	Date *birthdate;
     Email *email;
     Phone *phone;
     // the following to attributes are used in the linked list.
@@ -22,9 +22,9 @@ public:
     Person();
     ~Person();
     Person(string filename);
-    Person(string f_name, string l_name, /*string b_date,*/ string email, string phone);
+    Person(string f_name, string l_name, string b_date, string email, string phone);
 	void print_person();
-	// void set_person();
+	void set_person();
 	void set_person(string filename);
     bool operator==(const Person& rhs);
     bool operator!=(const Person& rhs);
