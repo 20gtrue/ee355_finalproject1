7757bd9e699ed4a61bd87c86c3d5e3f7c3668655
20gtrue <79179208+20gtrue@users.noreply.github.com>
2024-05-01 12:45:19 -0700

    Update contact.h

--- contact.h
+++ contact.h
@@ -1,48 +1,41 @@
 #ifndef CONTACT_H
 #define CONTACT_H
-//DONT INCLUDE PERSON!!!
-
 #include <iostream>
 #include <string>
-// TODO: You may need to add other libraries here!
 using namespace std;
 
-
-class Contact  {
-    // TODO: private or protected? Look at your children!	
+// Make class contact; virtual functions allow for child classes to call different versions of the parent functions
+class Contact  {	
 	protected: string type; // Private bc children need to access type
-public:
-    Contact() : type("") {}  // Default constructor   
-	virtual void print() = 0;
-    virtual string get_contact(string style="full") = 0;
-	virtual void set_contact() = 0;
+	public:
+	    	Contact() : type("") {}  // Default constructor   
+		virtual void print() = 0;
+	  	virtual string get_contact(string style="full") = 0;
+		virtual void set_contact() = 0;
 };
 
-
+// Make class Email, child of Contact
 class Email: public Contact{
-    protected: string email_addr;
-public:
-    Email() : email_addr("") {}  // Default constructor   
-    Email(string type, string email_addr);
-    // TODO: Add functions
-    virtual ~Email();
-    void print();
-    string get_contact(string style="full");
-    void set_contact();
+    	protected: string email_addr;
+	public:
+	    Email() : email_addr("") {}  // Default constructor   
+	    Email(string type, string email_addr);
+	    virtual ~Email();
+	    void print();
+	    string get_contact(string style="full");
+	    void set_contact();
 };
 
-
+// make class Phone, child of Contact
 class Phone: public Contact{
-    // TODO: modify dataType! Can int store 10 digit phone-number?
 	protected: string phone_num; 
-public:
-    Phone(string type, string phone_number);
-    Phone() : phone_num("") {}  // Default constructor
-    // TODO: Add functions
-    virtual ~Phone(); 
-    void print();
-    string get_contact(string style="full");
-    void set_contact();
+	public:
+	    Phone(string type, string phone_number);
+	    Phone() : phone_num("") {}  // Default constructor
+	    virtual ~Phone(); 
+	    void print();
+	    string get_contact(string style="full");
+	    void set_contact();
 };
 
 #endif
